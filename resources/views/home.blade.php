818835c8128811093f4a815209422de64478897b
MattFofo <matteo.segattini2@gmail.com>
2022-04-26 15:53:28 +0200

    stile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel primi passi</title>
+
+    <style>
+        .flex-center {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        ul {
+            text-align: center;
+            padding: 0;
+            margin: 0;
+        }
+        ul > li {
+            display: inline-block;
+        }
+    </style>
 </head>
-<body>
+<body class="flex-center">
     <header>
         <h1>{{ $type }} {{ $name }} with Laravel!</h1>
         <ul>
